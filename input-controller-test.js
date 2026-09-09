2ccb8a39b4234c8d71f27a14f742063df17f6cb5
Firefly10802 <miss.anni2002@mail.ru>
2026-09-09 13:18:44 +0300

    добавлена возможность управлять состояниями действий

--- input-controller-test.js
+++ input-controller-test.js
@@ -7,6 +7,12 @@ const controller = new InputController({
         keys: [39, 68]
     }
 })
-
+console.log(controller.isActionActive('left'))
+console.log(controller.disableAction('left'))
+console.log(controller.isActionActive('left'))
+console.log(controller.enableAction('left'))
+console.log(controller.isActionActive('left'))
+controller.pressedKey.add(65)
+console.log(controller.isActionActive('left'))
 console.log(controller)
 console.log(controller.actions)
--- input-controller.js
+++ input-controller.js
@@ -2,6 +2,7 @@
     class InputController {
         constructor(actionsToBind = {}) {
             this.actions = {}
+            this.pressedKey = new Set()
             this.bindActions(actionsToBind)
         }
 
@@ -9,12 +10,33 @@
             for (const actionName in actionsToBind) {
                 const action = actionsToBind[actionName]
                 const keys = [...new Set(action.keys)];
+
                 this.actions[actionName] = {
                     keys: keys,
                     enabled: action.enabled ?? true
                 };
             }
 
+        }
+
+        enableAction(actionName) {
+            if (this.actions[actionName]) {
+                this.actions[actionName].enabled = true;
+            }
+        }
+
+        disableAction(actionName) {
+            if (this.actions[actionName]) {
+                this.actions[actionName].enabled = false;
+            }
+        }
+
+        isActionActive(actionName) {
+            const action = this.actions[actionName];
+            if (!action || !action.enabled) {
+                return false;
+            }
+            return action.keys.some(key => this.pressedKey.has(key));
         }
     }
     window.InputController = InputController
